b792e4ade754cedd11c1eb7300c0f911fc82698f
Eric Z <6303703@bdeb.qc.ca>
2025-10-22 09:43:46 -0400

    Small adjustement in w11 graphic interface

--- src/GameManagement/PieceManagers.java
+++ src/GameManagement/PieceManagers.java
@@ -76,40 +76,40 @@ public class PieceManagers {
         int temp; //prints the number on the side of the chess board
         if(GameManager.getColor()) {
             temp = 8;
-            System.out.println("   A   B   C   D   E   F   G   H");
+            System.out.println("   A  B  C  D  E  F  G  H");
             for (int i = Board[0].length - 1; i >= 0; i--) {
-                System.out.print(temp + "   ");
+                System.out.print(temp + "  ");
                 for (int y = 0; y < Board[0].length; y++) {
                     if(Board[i][y] == '\u0000'){
-                        System.out.print(PieceManagers.Boardcolors[i][y] + "   ");
+                        System.out.print(PieceManagers.Boardcolors[i][y] + "  ");
                     } else {
-                        System.out.print(PieceIcon.transaltePieceIcon(Board[i][y]) + "   ");
+                        System.out.print(PieceIcon.transaltePieceIcon(Board[i][y]) + "  ");
                     }
                 }
-                System.out.print(temp + "   ");
+                System.out.print(temp + "  ");
                 temp--;
 
                 System.out.println();
             }
-            System.out.println("   A   B   C   D   E   F   G   H");
+            System.out.println("   A  B  C  D  E  F  G  H");
         } else {
             temp = 1;
-            System.out.println("   A   B   C   D   E   F   G   H");
+            System.out.println("   A  B  C  D  E  F  G  H");
             for (int i = 0; i < Board.length; i++) {
-                System.out.print(temp + "   ");
+                System.out.print(temp + "  ");
                 for (int y = 0; y < Board.length; y++) {
                     if(Board[i][y] == '\u0000'){
-                        System.out.print(PieceManagers.Boardcolors[i][y] + "   ");
+                        System.out.print(PieceManagers.Boardcolors[i][y] + "  ");
                     } else {
-                        System.out.print(PieceIcon.transaltePieceIcon(Board[i][y]) + "   ");
+                        System.out.print(PieceIcon.transaltePieceIcon(Board[i][y]) + "  ");
                     }
                 }
-                System.out.print(temp + "   ");
+                System.out.print(temp + "  ");
                 temp++;
 
                 System.out.println();
             }
-            System.out.println("   A   B   C   D   E   F   G   H");
+            System.out.println("   A  B  C  D  E  F  G  H");
         }
     }
 
